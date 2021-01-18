b9cc1f0857121e548c20916e689fb7f68566e843
Isnard Santos <isnard.santos@boavista.local>
2021-01-18 09:19:25 -0300

    incorrect password or userName

--- src/test/java/tests/FluxoBasico.java
+++ src/test/java/tests/FluxoBasico.java
@@ -23,8 +23,26 @@ public class FluxoBasico {
     }
 
     @Test
-    public void testUsuarioCadastrado(){
+    public void testUsuarioFirstPage(){
+        navegador.findElement(By.linkText("Sign in")).click();//colocar esse comando no before ? pq posso fazer
+        // conta nova ai como se comportar ? pensar...
+    }
+
+    @Test
+    public void testUsuarioLoginPage(){
         navegador.findElement(By.linkText("Sign in")).click();
+        navegador.findElement(By.id("login_field")).sendKeys("engisnard@gmail.com");
+        navegador.findElement(By.id("password")).sendKeys("aras2119");
+        navegador.findElement(By.className("btn-primary")).click();
+    }
+
+    @Test
+    public void testUsuarioValidandoUserNameIncorretoOuSenha(){
+        navegador.findElement(By.linkText("Sign in")).click();
+        navegador.findElement(By.className("btn-primary")).click();
+        WebElement alert = navegador.findElement(By.id("js-flash-container"));
+        String textoNoAlert = alert.getText();
+        assertEquals("Incorrect username or password.",textoNoAlert);
     }
 
     @After
